31afab54f77625cb7e25cc8a6e7215ffcae6e645
Addison Dittman <add99man2@yahoo.com>
2012-04-22 18:44:49 -0400

    commit after debug

--- CAHonline/CAHonline/Form1.cs
+++ CAHonline/CAHonline/Form1.cs
@@ -15,5 +15,10 @@ namespace CAHonline
         {
             InitializeComponent();
         }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            label1.Visible = false;
+        }
     }
 }
